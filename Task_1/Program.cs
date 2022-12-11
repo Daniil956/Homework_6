cbeaaa027ce09e2e9813488cab5b0ab8b865c913
Даниил <geimer_cok@mail.ru>
2022-12-11 14:36:50 +0300

    Finish

---
+++ Task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+System.Console.Write("Enter number of digits in massiv: ");
+int numberOfDigits = int.Parse(Console.ReadLine());
+int[] arrey = new int [numberOfDigits];
+int sum = 0;
+
+for (int i = 0; i < arrey.Length; i++)
+{
+    System.Console.Write("Enter the number under the index " + i + ": ");
+    arrey[i] = int.Parse(Console.ReadLine());
+    while (arrey[i] > 0)
+    {
+        sum +=1;
+    }
+}
+System.Console.WriteLine("[" + string.Join(", ", arrey) + "] -> " + sum);
---
+++ Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int EnteringVariables(string a)
+{
+    System.Console.Write(a);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+double b1 = EnteringVariables("Eneter number b1: ");
+double k1 = EnteringVariables("Eneter number k1: ");
+double b2 = EnteringVariables("Eneter number b2: ");
+double k2 = EnteringVariables("Eneter number k2: ");
+
+double x = (b2 - b1) / (k1 - k2);
+double y1 = k1 * x + b1;
+double y2 = k2 * x + b2;
+
+System.Console.WriteLine( y1 + "; " + y2 );
